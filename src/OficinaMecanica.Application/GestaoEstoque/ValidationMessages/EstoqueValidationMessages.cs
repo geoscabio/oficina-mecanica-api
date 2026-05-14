ec046898401e79ec8c370ab08f4771f2b75c94d7
geoscabio <geoscabio@gmail.com>
2026-05-14 03:32:50 -0300

    feat(application): add consultar item estoque use case

--- src/OficinaMecanica.Application/GestaoEstoque/ValidationMessages/EstoqueValidationMessages.cs
+++ src/OficinaMecanica.Application/GestaoEstoque/ValidationMessages/EstoqueValidationMessages.cs
@@ -7,4 +7,7 @@ public static class EstoqueValidationMessages
     public const string TamanhoPaginaInvalido = "Tamanho da página deve estar entre 1 e 100.";
     public const string PecaInsumoCatalogoObrigatorio = "Peça ou insumo do catálogo é obrigatório.";
     public const string QuantidadeMaiorQueZero = "Quantidade deve ser maior que zero.";
+    public const string ItemEstoqueNaoEncontrado = "Item de estoque não encontrado.";
+    public const string IdItemEstoqueObrigatorio = "Id do Item de estoque é obrigatório.";
+
 }
